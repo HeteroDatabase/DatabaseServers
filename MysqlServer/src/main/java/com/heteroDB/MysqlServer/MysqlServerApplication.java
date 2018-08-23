8189f57f02a12b4b0473f25a5af78421982e3410
shreypasari-accolite <shrey.pasari@accoliteindia.com>
2018-08-23 15:43:00 +0530

    Inital Commit

---
+++ MysqlServer/src/main/java/com/heteroDB/MysqlServer/MysqlServerApplication.java
@@ -0,0 +1,12 @@
+package com.heteroDB.MysqlServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlServerApplication.class, args);
+	}
+}
